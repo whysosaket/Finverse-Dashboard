bce9fd9196b616d83a5a3632bfcfb0aafd6957a8
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 23:15:50 +0530

    Configuration + Init Setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+
+  return (
+    <div className="">
+      <div className="">
+
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
