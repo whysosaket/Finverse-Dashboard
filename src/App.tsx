d588c14e684252369e577a807d715b98c21719eb
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-23 01:46:01 +0530

    Loading/Hireme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import CenterBar from "./components/CenterBar/CenterBar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Bar from "./components/RightBar/Bar";
+import Loading from "./components/Loading";
+import Hireme from "./components/Hireme";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      window.scrollTo(0, 0);
+    }, 1000);
+  }, []);
 
-
-  return (
+  return loading ? (
+    <>
+      <Loading />
+      <div className="opacity-0 w-1 h-1 overflow-hidden">
+        <div className="flex font-inter">
+          <div className="w-2/12 min-h-screen bg-white">
+            <Dashboard />
+          </div>
+          <div className="w-7/12 min-h-screen bg-gray-50">
+            <CenterBar />
+          </div>
+          <div className="w-3/12 min-h-screen bg-gray-100/80">
+            <Bar />
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
     <div className="flex font-inter">
+      <Hireme />
       <div className="w-2/12 min-h-screen bg-white">
         <Dashboard />
       </div>
@@ -17,7 +44,7 @@ function App() {
         <Bar />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
